1e40903429a1e528d7e9029e5d7204012e1a85c7
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-11-14 09:29:45 +0700

    feat: update docker-compose and add kong service by Ikhsan Satriadi
    Updates docker-compose.yml to include Kong service and
    configures database connections. Also adds init script.

---
+++ configs/init-main-db.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+
+    -- Initialize dataview database
+    DO \$\$
+    BEGIN
+       IF NOT EXISTS (
+          SELECT FROM pg_catalog.pg_roles
+          WHERE rolname = '${DATAVIEW_DB_USER}') THEN
+
+          CREATE USER ${DATAVIEW_DB_USER} WITH PASSWORD '${DATAVIEW_DB_PASSWORD}';
+       END IF;
+    END \$\$;
+
+    CREATE DATABASE ${DATAVIEW_DB_NAME};
+
+    GRANT ALL PRIVILEGES ON DATABASE ${DATAVIEW_DB_NAME} TO ${DATAVIEW_DB_USER};
+
+
+    -- Initialize kong database
+    DO \$\$
+    BEGIN
+       IF NOT EXISTS (
+          SELECT FROM pg_catalog.pg_roles
+          WHERE rolname = '${KONG_DB_USER}') THEN
+
+          CREATE USER ${KONG_DB_USER} WITH PASSWORD '${KONG_DB_PASSWORD}';
+       END IF;
+    END \$\$;
+
+    CREATE DATABASE ${KONG_DB_NAME};
+
+    GRANT ALL PRIVILEGES ON DATABASE ${KONG_DB_NAME} TO ${KONG_DB_USER};
+
+EOSQL
