1f900872288345d9b138cbb1072e7f3bba1ff4fe
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-11-20 09:11:45 +0700

    fix scipts

--- import-kong.sh
+++ import-kong.sh
@@ -1,3 +1,3 @@
-docker-compose exec deck deck gateway sync \
+docker compose exec deck deck gateway sync \
     --kong-addr http://dataverse_gateway:8086 \
     ./configs/kong.yml
--- run.sh
+++ run.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-git submodule update --init --recursive services
+docker compose down
 
 docker compose up -d --build
 
 # datahub migrate db
-docker-compose exec dataverse-datahub php artisan migrate --force
+docker compose exec dataverse-datahub php artisan migrate --force
 
 # dataview migrate db
-docker-compose exec dataverse-dataview php artisan migrate --force
+docker compose exec dataverse-dataview php artisan migrate --force
+
+# datacore migrate db
+docker compose exec dataverse-datacore bun run db:migrate
