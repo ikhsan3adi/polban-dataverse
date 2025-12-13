c38e7a3f43e223dff33936f6b6fb66d9787eb0e6
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-12-13 09:14:51 +0700

    refactor: streamline database setup and update configs
    Remove direct database initialization and migration for dataview.
    Update datacore-web environment variables to use Vite prefixes.
    Update polban-datacore and polban-datacore-api-docs submodules.

--- configs/init-main-db.sh
+++ configs/init-main-db.sh
@@ -4,20 +4,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 
-    -- Initialize dataview database
-    DO \$\$
-    BEGIN
-       IF NOT EXISTS (
-          SELECT FROM pg_catalog.pg_roles
-          WHERE rolname = '${DATAVIEW_DB_USER}') THEN
-
-          CREATE USER ${DATAVIEW_DB_USER} WITH PASSWORD '${DATAVIEW_DB_PASSWORD}';
-       END IF;
-    END \$\$;
-
-    CREATE DATABASE ${DATAVIEW_DB_NAME} WITH OWNER = ${DATAVIEW_DB_USER};
-
-
     -- Initialize kong database
     DO \$\$
     BEGIN
--- run.sh
+++ run.sh
@@ -9,8 +9,5 @@ docker compose up -d --build --remove-orphans
 # datahub migrate db
 docker compose exec dataverse-datahub php artisan migrate --force
 
-# dataview migrate db
-docker compose exec dataverse-dataview php artisan migrate --force
-
 # datacore migrate db
 # docker compose exec dataverse-datacore bun run db:migrate
